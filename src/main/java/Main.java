f71120b1453aa0ed655f6bce54a077ad191d956d
Jaimi Estrada <jaimiestrada007@gmail.com>
2023-03-24 15:27:35 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,172 @@
+import java.util.Random;
+
+public class Main {
+
+//    public static void printName(){
+//        String printName = "Jaimi Estrada";
+//        System.out.println(printName);
+
+//        public static String greetUser(String username){
+//        return "Welcome to my website" + username;
+//
+//    public static int buyCoffee(int dollars){
+//
+//    int change = dollars - 1;
+//    return change; }
+
+//    public static void myHobbies(String[] hobbies) {
+//        System.out.println("My hobbies are: ");
+//
+//        for (String hobby : hobbies) ;
+//        {
+//
+//            String hobby;
+//            System.out.println("-" + hobby);
+
+//    public static String checkWeather(String weathercondition, int temperature ){
+//
+//        if(weathercondition.equals("hot")&& temperature > 80){
+//
+//        return "wear shorts";}
+//
+//        else if(weathercondition.equals("cool") && temperature > 50){
+//
+//            return "wear wind breakers";}
+//        else if (weathercondition.equals("cold") && temperature < 50) {
+//
+//            return " wear a jacket ";
+//
+//        } else {
+//
+//            return "no such weather condition";
+//        }
+
+//    public static double areaOfTriangle(double base, double height){
+//
+//        double area = 0.5 * base * height;
+//        return area;
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+       // printName();
+
+//    String username = " jestrada";
+//
+//    String greeting = "Welcome to my website";
+//        System.out.println(greeting + username);
+
+//        int change = buyCoffee(2);
+//        System.out.println("buying coffee with $2 leaves me with " + change + "dollar" );
+
+//        String[] myHobbies = {"soccer", "gym", "traveling"};
+//        myHobbies(myHobbies);
+//    String outcome1 = checkWeather("hot", 90);
+//        System.out.println(outcome1);
+//    String outcome2 = checkWeather("cool",60 );
+//        System.out.println(outcome2);
+//    String outcome3 = checkWeather("cold", 45);
+//        System.out.println(outcome3);
+//        String outcome4 = checkWeather("raining", 65);
+//        System.out.println(outcome4);
+
+//        double outcome1 = areaOfTriangle(6.0, 4.0);
+//        System.out.println(outcome1);
+
+        Random rand1 = new Random();
+        int num = rand1.nextInt(6) + 1;
+
+        switch(num){
+            case 1:
+                System.out.println("you rolled a one!");
+                break;
+            case 2:
+                System.out.println("you rolled a two!");
+                break;
+            case 3:
+                System.out.println("you rolled a three");
+                break;
+            case 4:
+                System.out.println("you rolled a four");
+                break;
+            case 5:
+                System.out.println("you rolled a five");
+                break;
+            case 6:
+                System.out.println("you rolled a six");
+                break;
+             case
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
